f756a774651529f5f7d85403db783300de8468d9
Lilia Heinold <lilia_heinold@student.uml.edu>
2019-01-19 16:42:56 -0500

    fixing weird merge problems :(

--- app/routes.py
+++ app/routes.py
@@ -29,10 +29,6 @@ def index():
 def get_github_token():
 	return session.get('github_token')
 
-@app.route('/login-github')
-def login():
-	return github.authorize(callback='http://localhost:5000/github-authorized') #callback=url_of('oauth_authorized')
-
 @app.route('/github-authorized')
 @github.authorized_handler
 def oauth_authorized(resp):
@@ -57,31 +53,9 @@ def oauth_authorized(resp):
 @app.route('/form')
 def form():
     return render_template('form.html')
-@github.tokengetter
-def get_github_token():
-	return session.get('github_token')
+
 
 @app.route('/login-github')
 def login():
 	return github.authorize(callback='http://localhost:5000/github-authorized') #callback=url_of('oauth_authorized')
 
-@app.route('/github-authorized')
-@github.authorized_handler
-def oauth_authorized(resp):
-	next_url = request.args.get('next') or url_for('index')
-	if resp is None:
-		print('You denied the request to sign in.')
-		return redirect(next_url)
-
-	session['github_token'] = (
-		resp['access_token'],
-		'cat'
-	)
-	user = github.get("https://api.github.com/user", headers={'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': 'token ' + resp['access_token']})
-	session['github_user'] = user.data['login']
-
-	if (mongo.db.userData.find({"userName": user.data['login']}).count() == 0):
-		mongo.db.userData.insert_one({"userName": user.data['login'], "tags": [], "duckDuckCoins": 0})
-
-	print('You were signed in as %s' % session['github_user'])
-	return redirect(next_url)
