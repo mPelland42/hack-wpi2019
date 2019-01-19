81739ec2c1c3704b95cb4cf4dee8444ece90d28d
mpelland42 <mpelland42@gmail.com>
2019-01-19 00:50:53 -0500

    wip

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
 
 from flask import render_template
+from flask_pymongo import PyMongo
 from app import app
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/ddc"
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username': 'matt'}
+	user = mongo.db.Users.find({})[0]
 	return render_template('index.html', title="Home", user=user)
