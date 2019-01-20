2a2fb719152aaf3d737bdaaf913cf985bbf2ac7b
Jackson Seal <sealj553@gmail.com>
2019-01-20 01:06:52 -0500

    improved front page theme

--- app/templates/header.js
+++ app/templates/header.js
@@ -5,6 +5,18 @@ import Duckon from './images/duck.png'
 
 import {FormControl, FormGroup, ControlLabel, HelpBlock} from "react-bootstrap"
 
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
+$(document).ready( function () {
+    $('#down').on('click',function () {
+    $("html").scrollTop(0);
+    $('html, body').animate({
+        scrollTop: $(".main").offset().top }, 1000);
+    });
+});
+
 
 class Header extends React.Component {
   constructor(props, context) {
@@ -38,6 +50,9 @@ class Header extends React.Component {
           </div>
         </div>
       </div>
+
+      <span id="down">&#8595;</span>
+
     </header>
     );
   }
